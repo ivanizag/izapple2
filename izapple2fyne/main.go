17f11219807f89b9e11c512a98e43ad765b5bf3d
Ivan Izaguirre <ivanizag@gmail.com>
2020-10-11 20:31:55 +0200

    Toolbar commands

--- izapple2fyne/main.go
+++ izapple2fyne/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"image"
 	"time"
 
 	"github.com/ivanizag/izapple2"
@@ -12,42 +14,90 @@ import (
 	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/driver/desktop"
 	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 )
 
+type state struct {
+	a *izapple2.Apple2
+
+	showPages bool
+}
+
 func main() {
-	a := izapple2.MainApple()
-	if a != nil {
-		if a.IsProfiling() {
+	var s state
+	s.a = izapple2.MainApple()
+	if s.a != nil {
+		if s.a.IsProfiling() {
 			// See the log with:
 			//    go tool pprof --pdf ~/go/bin/izapple2sdl /tmp/profile329536248/cpu.pprof > profile.pdf
 			defer profile.Start().Stop()
 		}
 
-		fyneRun(a)
+		fyneRun(&s)
 	}
 }
 
-func fyneRun(a *izapple2.Apple2) {
+func fyneRun(s *state) {
 	app := app.New()
 	// app.SetIcon(xxx)
-	window := app.NewWindow("iz-" + a.Name)
+	window := app.NewWindow("iz-" + s.a.Name)
 	// window.SetIcon(xxx)
 
+	bottom := widget.NewToolbar(
+		widget.NewToolbarAction(
+			theme.NewThemedResource(resourceRestartSvg, nil), func() {
+				s.a.SendCommand(izapple2.CommandReset)
+			}),
+		widget.NewToolbarAction(
+			theme.NewThemedResource(resourcePauseSvg, nil), func() {
+				s.a.SendCommand(izapple2.CommandPauseUnpauseEmulator)
+			}),
+		widget.NewToolbarAction(
+			theme.NewThemedResource(resourceFastForwardSvg, nil), func() {
+				s.a.SendCommand(izapple2.CommandToggleSpeed)
+			}),
+		widget.NewToolbarSeparator(),
+		widget.NewToolbarAction(
+			theme.NewThemedResource(resourcePaletteSvg, nil), func() {
+				s.a.SendCommand(izapple2.CommandToggleColor)
+			}),
+		widget.NewToolbarAction(
+			theme.NewThemedResource(resourceLayersTripleSvg, nil), func() {
+				s.showPages = !s.showPages
+				if !s.showPages {
+					window.SetTitle("iz-" + s.a.Name)
+				}
+			}),
+		widget.NewToolbarAction(
+			theme.NewThemedResource(resourceCameraSvg, nil), func() {
+				err := izapple2.SaveSnapshot(s.a, "snapshot.png")
+				if err != nil {
+					app.SendNotification(fyne.NewNotification(window.Title(),
+						fmt.Sprintf("Error saving snapshoot: %v.\n.", err)))
+				} else {
+					app.SendNotification(fyne.NewNotification(window.Title(),
+						"Saving snapshot on 'snapshot.png'"))
+				}
+			}),
+		widget.NewToolbarSpacer(),
+		widget.NewToolbarAction(theme.ViewFullScreenIcon(), func() {
+			window.SetFullScreen(!window.FullScreen())
+		}),
+	)
+
 	screen := canvas.NewImageFromImage(nil)
-	top := widget.NewLabel("Top")
-	bottom := widget.NewLabel("Bottom")
-	right := widget.NewLabel("Right")
+	screen.SetMinSize(fyne.NewSize(380, 192))
 	container := fyne.NewContainerWithLayout(
-		layout.NewBorderLayout(top, bottom, nil, right),
-		screen, top, bottom, right,
+		layout.NewBorderLayout(nil, bottom, nil, nil),
+		screen, bottom,
 	)
 	window.SetContent(container)
 	window.SetPadded(false)
 
-	registerKeyboardEvents(a, window.Canvas())
+	registerKeyboardEvents(s, window.Canvas())
 
-	go a.Run()
+	go s.a.Run()
 
 	ticker := time.NewTicker(60 * time.Millisecond)
 	done := make(chan bool)
@@ -57,8 +107,14 @@ func fyneRun(a *izapple2.Apple2) {
 			case <-done:
 				return
 			case <-ticker.C:
-				if !a.IsPaused() {
-					img := a.Snapshot()
+				if !s.a.IsPaused() {
+					var img *image.RGBA
+					if s.showPages {
+						img = s.a.SnapshotParts()
+						window.SetTitle(fmt.Sprintf("%v %v %vx%v", s.a.Name, s.a.VideoModeName(), img.Rect.Dx()/2, img.Rect.Dy()/2))
+					} else {
+						img = s.a.Snapshot()
+					}
 					screen.Image = img
 					canvas.Refresh(screen)
 				}
@@ -75,8 +131,8 @@ func fyneRun(a *izapple2.Apple2) {
 
 }
 
-func registerKeyboardEvents(a *izapple2.Apple2, canvas fyne.Canvas) {
-	kp := newKeyboard(a)
+func registerKeyboardEvents(s *state, canvas fyne.Canvas) {
+	kp := newKeyboard(s)
 
 	// Koyboard events
 	canvas.SetOnTypedKey(func(ke *fyne.KeyEvent) {
---
+++ izapple2fyne/notes.md
@@ -0,0 +1,13 @@
+# Fyne
+
+- Tooltip on widgets. As there is a Hoverable, it would be nice to have Tooltipable¿? and check on mouse move.
+```
+type Tooltipable interface {
+	Tooltip() string
+}
+```
+
+- Text on the toolabar items, at least tooltips
+- Missing keys for Pause and for PrintScreen.
+- RunOnMain()
+- Buttons with state: pressed, disabled
